e6530ade7b15c4ab0368ea58123debc34b24f9f1
doo-dev <nimo.vimo.yank@gmail.com>
2024-02-23 10:20:55 +0330

    feat: add config and mysql client

---
+++ infra/mysql/client.go
@@ -0,0 +1,37 @@
+package mysql
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"github.com/go-sql-driver/mysql"
+	"time"
+)
+
+func NewClient(ctx context.Context, cfg *Config) (*sql.DB, error) {
+	loc, err := time.LoadLocation(cfg.Timezone)
+	if err != nil {
+		return nil, err
+	}
+
+	c := mysql.Config{
+		User:            cfg.Username,
+		Passwd:          cfg.Password,
+		Net:             "tcp",
+		Addr:            fmt.Sprintf("%s:%d", cfg.Host, cfg.Port),
+		DBName:          cfg.DatabaseName,
+		Loc:             loc,
+		MultiStatements: true,
+	}
+
+	db, err := sql.Open("mysql", c.FormatDSN())
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
